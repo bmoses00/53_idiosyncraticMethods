6e7e3c2a8d50720d72dbf0320edaa39eb802e025
Jimmy Luo <jluo00@stuy.edu>
2019-03-18 15:48:28 +0900

    added Brian's works

--- Accumulator.java
+++ Accumulator.java
@@ -10,22 +10,117 @@ public class Accumulator {
       in the \list that begin with \prefix,
       each followed by a space.
      */
-    String catElementsStartingWith(
+    public static String catElementsStartingWith(
         List_inArraySlots list
       , String prefix
       ) {
-        String result;
-        result = "change me";
-        return result; 
+        String result = "";
+
+        for( int el = 0; el < list.size(); el++) {
+
+            /* 3.  Stumbling block 0
+               Java protects a programmer against applying a method to
+               elements in list when some elements of the list might
+               omit support for a particular operation.
+
+               This protection is implemented by the compiler
+               The following code violates the restriction:
+              */
+            // if (list.get(el).startsWith(prefix)) result += list.get(el);
+            /*
+             predicted error message: method not found in class
+
+             actual error message:
+
+             .\Accumulator.java:32: error: cannot find symbol
+                             if (list.get(el).startsWith(prefix));
+                                             ^
+               symbol:   method startsWith(String)
+               location: class Object
+             1 error
+
+            */
+            /* 4.  Workaround 0
+               A programmer should expect there to be a way to
+               work around the stumbling block, because
+               the compiler knows the type of an element.
+
+               Java's instanceof operator identifies the type
+               of an element to the compiler.
+             */
+
+
+            /* 5.  Stumbling block 1
+               However, use of the operator alone is insufficient,
+               because the compiler objects to the following code
+               that adds use of the operator to the code from
+               Stumbling block 0:
+             */
+
+            /*
+                if (list.get(el) instanceof String) {
+                    if (list.get(el).startsWith(prefix)) {
+                        result += list.get(el);
+                    }
+                }
+
+             predicted error message: method not found in class
+
+             actual error message:
+
+             .\Accumulator.java:64: error: cannot find symbol
+                                 if (list.get(el).startsWith(prefix)) {
+                                                 ^
+               symbol:   method startsWith(String)
+               location: class Object
+             1 error
+             */
+
+            /* 6. Workaround 1
+               Programmers use Java's cast operator
+               to tell the JVM that code uses a subclass's method
+               on an object, even though the reference to the
+               object is stored in a super-class variable.
+             */
+
+            /* 7. Stumbling block 2
+               However, use of this operator alone is insufficient,
+               because the JVM objects to the following code that
+               adds use of the operator to the code from Stumbling
+               block 0:
+
+            if ( ((String)list.get(el)).startsWith(prefix) )
+                result += list.get(el);
+
+             predicted error message: cannot cast one type to another
+
+             actual error message:
+
+             Exception in thread "main" java.lang.ClassCastException:
+             java.base/java.lang.Double cannot be cast to java.base/java.lang.String
+                     at Accumulator.catElementsStartingWith(Accumulator.java:95)
+                     at UserOfList.main(UserOfList.java:32)
+            */
+
+            /* 8. Workaround 2
+               A programmer can combine use of both operators
+               to apply a method to exactly those
+               elements in the list that support the method.
+             */
+          if (list.get(el) instanceof String) {
+              String temp = (String) list.get(el);
+              if ( temp.startsWith(prefix) )
+                  result += temp + ", ";
+            }
+        }
+        return result;
     }
 
+    /**
+      @return a list of each of the Double elements
+      from the \list whose value is "finite".
+     */
+    public static List_inArraySlots finites(List_inArraySlots list) {
 
-    // /**
-      // @return a list of each of the Double elements
-      // from the \list whose value is "finite".
-     // */
-    // public List_inArraySlots finites(
-        // List_inArraySlots list
-      // ) {
-    // }
+    }
 }
--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,63 +1,56 @@
 /**
-  Implement a list of diverse types, including
-  integers, double-precision floating point numbers,
-  and Strings.
+  Implement a list of diverse types.
  */
-// CREDIT: Based off of Amy Lei and Kyle Sacks's work with Element[] array
 
 public class List_inArraySlots {
-    // FIELDS
 
     private int filledElements; // the number of elements in this list
     private Object[] list;
 
     private static final int INITIAL_CAPACITY = 10;
 
-    // CONSTRUCTORS
-
+    /**
+      Construct an empty list with a small initial capacity.
+     */
     public List_inArraySlots() {
-        list = new Object[INITIAL_CAPACITY];
+      list = new Object[ INITIAL_CAPACITY];
     }
 
-    // METHODS
 
     /**
       @return the number of elements in this list
      */
     public int size() {
-        return filledElements;
+      return filledElements;
     }
+
 
      /**
        @return a string representation of this list,
        in [a,b,c,] format
       */
     public String toString() {
-        String stringRep = "[";
+      String stringRep = "[";
 
-        for (int index = 0; index < filledElements; index++)
-            stringRep += list[index] + ",";
+      for (int index = 0; index < filledElements; index++)
+        stringRep += list[index] + ",";
 
-        stringRep += "]";
-        return stringRep;
+      stringRep += "]";
+      return stringRep;
     }
 
 
     /**
       Appends @value to the end of this list.
+
       @return true, in keeping with conventions yet to be discussed
      */
-     public boolean add(Object input) {
-          if (filledElements == list.length) expand();
-
-          list[filledElements] = input;
-          filledElements++;
-          return true;
+     public boolean add( Object val) {
+        // expand when at max
+        if (filledElements == list.length) expand();
+        list[ filledElements++] = val;
+        return true;
      }
-
-    public Object get(int index){
-        return list[index];
-    }
 
 
     /**
@@ -65,13 +58,19 @@ public class List_inArraySlots {
       preserving existing data.
      */
      private void expand() {
-         //System.out.println( "old length:" + typeOfElements.length);
-         // create new list, copy values over, and replace list
-         Object[] bigger = new Object[list.length * 2];
-         for(int index=0;index<list.length; index++){
-             bigger[index] = list[index];
-         }
-         list = bigger;
-         // System.out.println( "new length:" + typeOfElements.length);
+        // create new list, copy values over, and replace list
+        Object[] bigger = new Object[list.length * 2];
+
+        // copy existing elements
+        for(int index=0; index<list.length; index++)
+            bigger[index] = list[index];
+
+        list = bigger;
      }
+
+
+     public Object get( int index){
+        // pass-through request
+        return list[ index];
+    }
 }
